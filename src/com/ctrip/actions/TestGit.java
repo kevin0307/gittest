49717d1f858d2d4670766dcfcd42493dd5c8215c
kevin0307 <summitlylee@sina.com>
2013-10-23 11:20:37 +0800

    add a testGit.java

---
+++ src/com/ctrip/actions/TestGit.java
@@ -0,0 +1,20 @@
+package com.ctrip.actions;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: xf_li
+ * Date: 13-10-23
+ * Time: 上午11:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestGit {
+
+        public static void main(String args[]){
+
+            //print something about git.
+            System.out.print("Git is a version controller!");
+
+        }
+
+
+}
